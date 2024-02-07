6c9c7805b17dffa0a9a13c577fa22ae621a6f0ae
boopathivenkat <159232751+boopathivenkat@users.noreply.github.com>
2024-02-07 12:20:35 +0530

    Add files via upload

---
+++ learn.py
@@ -0,0 +1,14 @@
+a=input ("Your Name:")
+print("Hello",a,"Welcome to Our Company")
+b=int(input ("Enter Your Age:"))
+if (b>=18 and b<=35):
+    d=int (input ("Enter Your Mobile No:"))
+    input ("Enter Your Location:")
+    e=("Save success Your details... I will call your Number:",d)
+else:
+    f=("Sorry...Yor Are Not elegible For Our Company")
+    print(f.upper())   
+    
+
+ 
+
